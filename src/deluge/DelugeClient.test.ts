bedc011a3d932a7b3659896d49ca30486feefe5f
vinz243 <vinz243@gmail.com>
2017-10-22 16:37:52 +0200

    add remove

--- src/deluge/DelugeClient.test.ts
+++ src/deluge/DelugeClient.test.ts
@@ -20,7 +20,9 @@ test('DelugeClient.connect() - connects to a daemon', async (t) => {
 test('DelugeClient.getTorrents() - returns an empty list', async (t) => {
   const client = new DelugeClient({host: 'localhost', port: 8112, password: 'deluge'});
   await client.connect();
-  await client.getTorrents();
+  const torrents = await client.getTorrents();
+
+  t.is(torrents.length, 0, 'should return empty list');
   t.end();
 });
 
@@ -34,12 +36,13 @@ test('DelugeClient.addFiles() - adds torrents', async (t) => {
   const ubuntu_torrent = path.join(__dirname, '../../torrents/ubuntu-17.10-desktop-amd64.iso.torrent');
 
   const torrents = await client.addFiles([archlinux_torrent, await readFile(ubuntu_torrent, {encoding: null})], {
-    directory: path.join(__dirname, '../../torrents/target')
+    directory: path.join(__dirname, '../../torrents/target'),
+    tempDirectory: path.join(__dirname, '../../torrents/temp')
   });
 
-  t.is(torrents.length, 2);
-  t.is(torrents[0].hash, '9228628504cc40efa57bf38e85c9e3bd2c572b5b');
-  t.is(torrents[1].hash, '40448d478d9203a3919b0900e7fbb9e8748dcdf9');
+  t.is(torrents.length, 2, 'should return 2 torrents');
+  t.is(torrents[0].hash, '9228628504cc40efa57bf38e85c9e3bd2c572b5b', 'should have the right hashes');
+  t.is(torrents[1].hash, '40448d478d9203a3919b0900e7fbb9e8748dcdf9', 'should have the right hashes')
 
   t.end();
 });
@@ -51,10 +54,40 @@ test('DelugeClient.getTorrents() - returns the newly added torrents', async (t) 
 
   const torrents = await client.getTorrents();
 
-  t.is(torrents.length, 2);
+  t.is(torrents.length, 2, 'should only return two torrents');
 
-  t.is(torrents[0].name, 'ubuntu-17.10-desktop-amd64.iso');
-  t.is(torrents[1].name, 'archlinux-2017.10.01-x86_64.iso');
+  t.is(torrents[0].name, 'ubuntu-17.10-desktop-amd64.iso', 'should have the torrent name');
+  t.is(torrents[1].name, 'archlinux-2017.10.01-x86_64.iso', 'should have the torrent name');
+
+  t.end();
+});
+
+test('DelugeTorrent.remove() - removes torrents with data', async (t) => {
+  const readdir = util.promisify(fs.readdir);
+  const unlink  = util.promisify(fs.unlink);
+
+  const client = new DelugeClient({host: 'localhost', port: 8112, password: 'deluge'});
+
+  await client.connect();
+
+  const torrents = await client.getTorrents();
+  t.is(torrents.length, 2, 'should return all the torrents');
+
+  await Promise.all(torrents.map((t) => t.remove(true)));
+
+  const emptyTorrents = await client.getTorrents();
+  t.is(emptyTorrents.length, 0, 'should return no torrents once removed');
+
+  const target = path.join(__dirname, '../../torrents/target');
+  const temp = path.join(__dirname, '../../torrents/temp');
+
+  const downloads = await readdir(target);
   
+  t.deepEqual(downloads, ['.gitignore'], 'should have removed the downloaded files');
+
+  const temps = await readdir(temp);
+  const ops = temps.filter((f) => f !== '.gitignore').map((f) => unlink(path.join(temp, f)));
+  await Promise.all(ops);
+
   t.end();
 });
--- src/deluge/DelugeTorrent.ts
+++ src/deluge/DelugeTorrent.ts
@@ -1,12 +1,13 @@
 import Torrent, {TorrentProps} from '../models/Torrent';
 import Options from './options';
+import Request from './DelugeClientRequest';
+import ClientError from '../models/ClientError';
 
 export default class DelugeTorrent extends Torrent<Options> {
   constructor (opts: Options, hash: string, props: TorrentProps) {
     super(opts, hash, props);
-
   }
-  liveFeed(): void {
+  liveFeed(polling = 420): void {
     throw new Error("Method not implemented.");
   }
   update(): Promise<void> {
@@ -15,8 +16,14 @@ export default class DelugeTorrent extends Torrent<Options> {
   pause(): Promise<void> {
     throw new Error("Method not implemented.");
   }
-  remove(deleteData: boolean): Promise<void> {
-    throw new Error("Method not implemented.");
+  async remove (deleteData: boolean): Promise<void> {
+    const req = new Request(this.opts);
+    req.add('deleted', 'core.remove_torrent', [this._hash, deleteData]);
+    const {deleted} = await req.send();
+
+    if (!deleted) {
+      throw new ClientError('server_error', 'Unable to delete torrent');
+    }
   }
 
 }
